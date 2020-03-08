ee00e74a3115a187d1321077f87751dc053c43bb
Denis Vasilik <contact@denisvasilik.com>
2020-03-08 10:07:11 +0100

    Update copyright date

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 
 project = u"Binalyzer"
-copyright = u"2019 Denis Vasilìk"
+copyright = u"2020 Denis Vasilìk"
 author = u"Denis Vasilìk"
 
 # The short X.Y version
