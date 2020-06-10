4f063949fd71dcb9772f42c0654cc60fb390cdb9
Denis Vasilik <contact@denisvasilik.com>
2020-06-10 22:32:45 +0200

    Remove Antlr generator

--- ci/run.sh
+++ ci/run.sh
@@ -20,12 +20,4 @@ then
     sed -i 's/__commit__ = \"00000000\"/__commit__ = \"'${TRAVIS_COMMIT::6}'\"/g' binalyzer/__init__.py
 fi
 
-curl -O https://www.antlr.org/download/antlr-4.8-complete.jar
-java -jar antlr-4.8-complete.jar \
-        -Dlanguage=Python3 \
-        resources/XMLLexer.g4 \
-        resources/XMLParser.g4
-mv resources/*.py binalyzer/generated
-rm antlr-4.8-complete.jar
-
 python3 -m pytest tests --cov=binalyzer --cov-fail-under=20
