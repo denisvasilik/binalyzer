a43875d4a98df5c951e3e224135f786a41ecaf21
Denis Vasilik <contact@denisvasilik.com>
2020-06-11 11:31:08 +0200

    Reexport binalyzer and TemplateAutoCompletion

--- binalyzer/__init__.py
+++ binalyzer/__init__.py
@@ -18,6 +18,8 @@ __commit__ = "00000000"
 
 from binalyzer_core import (
     Binalyzer,
+    TemplateProvider,
+    DataProvider,
     BindingContext,
     ByteOrder,
     AddressingMode,
@@ -27,4 +29,4 @@ from binalyzer_core import (
 )
 from binalyzer_template_provider import XMLTemplateParser, XMLTemplateFileParser
 from binalyzer_data_provider import BufferedIODataProvider
-from binalyzer_cli import cli
+from binalyzer_cli import binalyzer, TemplateAutoCompletion
