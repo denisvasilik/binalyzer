dbaecaf50fecb7caeb05ba233d503ce9aff9f0fc
Denis Vasilik <contact@denisvasilik.com>
2020-07-14 09:47:45 +0200

    Add module comment

--- binalyzer/cli.py
+++ binalyzer/cli.py
@@ -1,15 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+    binalyzer.cli
+    ~~~~~~~~~~~~~
+
+    This module implements the entry point used by the command line interface.
+
+    :copyright: 2020 Denis Vasilík
+    :license: MIT, see LICENSE for details.
+"""
 import sys
 
 from binalyzer_cli.cli import BinalyzerGroup
 
 
-cli = BinalyzerGroup(help="")
+cli = BinalyzerGroup()
 cli.name = "binalyzer"
 
 
-def main(as_module=False):
+def main():
     cli.main(args=sys.argv[1:])
 
 
 if __name__ == "__main__":
-    main(as_module=True)
+    main()
