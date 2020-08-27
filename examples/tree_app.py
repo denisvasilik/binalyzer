6ad56e7c298113c6f69ababe4cff2172f8acd1ac
Denis Vasilik <contact@denisvasilik.com>
2020-08-27 19:00:17 +0200

    Update resources path

--- examples/tree_app.py
+++ examples/tree_app.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     binalyzer = Binalyzer()
     cwd_path = os.path.dirname(os.path.abspath(__file__))
     binalyzer.xml.from_file(
-        os.path.join(cwd_path, "resources/wasm_module.xml"),
-        os.path.join(cwd_path, "resources/wasm_module.wasm"),
+        os.path.join(cwd_path, "../resources/wasm_module.xml"),
+        os.path.join(cwd_path, "../resources/wasm_module.wasm"),
     )
 
     for pre, fill, node in RenderTree(binalyzer.template):
