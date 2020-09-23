7581b89869c08c77fdbc43e68d5831124bc94cbf
Denis Vasilik <contact@denisvasilik.com>
2020-09-23 14:13:12 +0200

    Clean up

--- examples/pcap_app.py
+++ examples/pcap_app.py
@@ -4,16 +4,21 @@ import os
 from binalyzer import Binalyzer
 
 cwd_path = os.path.dirname(os.path.abspath(__file__))
+resources_path = os.path.join(cwd_path, "../resources")
 
 pcap = Binalyzer().xml.from_file(
-    os.path.join(cwd_path, "../resources/pcapng.xml"),
-    os.path.join(cwd_path, "../resources/network_trace.pcap"),
+    os.path.join(resources_path, "pcapng.xml"),
+    os.path.join(resources_path, "network_trace.pcap"),
 )
 ethernet_frame = Binalyzer().xml.from_file(
-    os.path.join(cwd_path, "../resources/ethernet_frame.xml")
+    os.path.join(resources_path, "ethernet_frame.xml")
 )
-ipv4 = Binalyzer().xml.from_file(os.path.join(cwd_path, "../resources/ipv4.xml"))
-tcp = Binalyzer().xml.from_file(os.path.join(cwd_path, "../resources/tcp.xml"))
+ipv4 = Binalyzer().xml.from_file(
+    os.path.join(resources_path, "ipv4.xml")
+)
+tcp = Binalyzer().xml.from_file(
+    os.path.join(resources_path, "tcp.xml")
+)
 
 for i, packet_record in enumerate(pcap.packet_records):
     ethernet_frame.value = packet_record.packet_data.value
