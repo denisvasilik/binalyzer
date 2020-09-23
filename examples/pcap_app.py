6ff65313054d0134167f8a2b69098a2ea3df0190
Denis Vasilik <contact@denisvasilik.com>
2020-09-23 14:07:43 +0200

    Iterate over packet records

--- examples/pcap_app.py
+++ examples/pcap_app.py
@@ -15,10 +15,12 @@ ethernet_frame = Binalyzer().xml.from_file(
 ipv4 = Binalyzer().xml.from_file(os.path.join(cwd_path, "../resources/ipv4.xml"))
 tcp = Binalyzer().xml.from_file(os.path.join(cwd_path, "../resources/tcp.xml"))
 
-ethernet_frame.value = pcap.packet_record_0.packet_data.value
-ipv4.value = ethernet_frame.payload.value
-tcp.value = ipv4.payload.value
+for i, packet_record in enumerate(pcap.packet_records):
+    ethernet_frame.value = packet_record.packet_data.value
+    ipv4.value = ethernet_frame.payload.value
+    tcp.value = ipv4.payload.value
 
-print("Source port: {}".format(int.from_bytes(tcp.source_port.value, "big")))
-print("Destination port: {}".format(int.from_bytes(tcp.destination_port.value, "big")))
-print("Sequence number: {}".format(int.from_bytes(tcp.sequence_number.value, "big")))
+    print(f"--- Packet Record {i} ---")
+    print(f"Source port: {int.from_bytes(tcp.source_port.value, 'big')}")
+    print(f"Destination port: {int.from_bytes(tcp.destination_port.value, 'big')}")
+    print(f"Sequence number: {int.from_bytes(tcp.sequence_number.value, 'big')}")
