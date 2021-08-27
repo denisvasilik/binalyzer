ef967b0be45e69af1c694a98e108a8587ea2675e
Denis Vasilik <contact@denisvasilik.com>
2021-08-28 01:27:50 +0200

    Recreate count_property for reference value change

--- examples/dynamic_template_app.py
+++ examples/dynamic_template_app.py
@@ -1,6 +1,10 @@
 import os
 
-from binalyzer import Binalyzer
+from binalyzer import (
+    Binalyzer,
+    Template,
+    ReferenceProperty,
+)
 
 cwd_path = os.path.dirname(os.path.abspath(__file__))
 resources_path = os.path.join(cwd_path, "../resources")
@@ -11,12 +15,14 @@ data_filepath = os.path.join(resources_path, "dynamic_template_example.bin")
 binalyzer = Binalyzer().xml.from_file(template_filepath)
 
 template = binalyzer.template
+
 template.num_fields.value = bytes([0x03])
+template.fields.children = []
 
-# Workaround
-binalyzer.template.binding_context._invalidate_dom()
-template = binalyzer.template
+template_field = Template(name="field", parent=template.fields)
+template_field.size = 2
+template_field.count_property = ReferenceProperty(template_field, "num_fields")
 
-print(f"Total template size in bytes: {template.size}")
-print(f"Total data size in bytes: {len(template.value)}")
-print(f"Number of fields: {len(template.fields.children)}")
+print(f"Total template size in bytes: {binalyzer.template.size}")
+print(f"Total data size in bytes: {len(binalyzer.template.value)}")
+print(f"Number of fields: {len(binalyzer.template.fields.children)}")
