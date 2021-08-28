fef06017c7a9583cdf791b18aae864b1a8afa79d
Denis Vasilik <contact@denisvasilik.com>
2021-08-28 11:32:16 +0200

    Update dynamic template expansion example

---
+++ binalyzer/examples.py
@@ -0,0 +1,17 @@
+import os
+
+CWD_PATH = os.path.dirname(os.path.abspath(__file__))
+RESOURCES_PATH = os.path.join(CWD_PATH, "../resources")
+
+
+class DynamicTemplateExpansion():
+
+    TEMPLATE_FILEPATH = os.path.join(
+        RESOURCES_PATH,
+        "dynamic_template_example.xml"
+    )
+
+    DATA_FILEPATH = os.path.join(
+        RESOURCES_PATH,
+        "dynamic_template_example.bin"
+    )
--- examples/dynamic_template_app.py
+++ examples/dynamic_template_app.py
@@ -6,23 +6,29 @@ from binalyzer import (
     ReferenceProperty,
 )
 
-cwd_path = os.path.dirname(os.path.abspath(__file__))
-resources_path = os.path.join(cwd_path, "../resources")
+from binalyzer.examples import DynamicTemplateExpansion
 
-template_filepath = os.path.join(resources_path, "dynamic_template_example.xml")
-data_filepath = os.path.join(resources_path, "dynamic_template_example.bin")
 
-binalyzer = Binalyzer().xml.from_file(template_filepath)
+binalyzer = Binalyzer().xml.from_file(
+    DynamicTemplateExpansion.TEMPLATE_FILEPATH
+)
 
-template = binalyzer.template
+# Update `num_fields`
+binalyzer.template.num_fields.value = bytes([0x07])
 
-template.num_fields.value = bytes([0x03])
-template.fields.children = []
-
-template_field = Template(name="field", parent=template.fields)
+# The `count` attribute must be set again, because the above `binalyzer.template`
+# statement already expanded the tree using the preset `num_fields` value read 
+# from the binary file.
+# Thus, the existing children of the `fields` template must be first removed and
+# a new template `field` must be created, referencing the `num_fields` template.
+binalyzer.template.fields.children = []
+template_field = Template(name="field", parent=binalyzer.template.fields)
 template_field.size = 2
 template_field.count_property = ReferenceProperty(template_field, "num_fields")
 
 print(f"Total template size in bytes: {binalyzer.template.size}")
 print(f"Total data size in bytes: {len(binalyzer.template.value)}")
 print(f"Number of fields: {len(binalyzer.template.fields.children)}")
+
+with open(DynamicTemplateExpansion.DATA_FILEPATH, 'wb') as data_file:
+    data_file.write(binalyzer.template.value)
